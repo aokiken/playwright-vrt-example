287270c33774cb67b20071eb825d870fa53b2ece
aokiken <aokiken@me.com>
2022-08-02 21:44:45 +0900

    シンプルなVRTテストコードを追加

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // create a locator
-  const getStarted = page.locator('text=Get Started');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+test('こんにちは世界', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  await expect(page).toHaveScreenshot({fullPage: true, scale: 'device', animations: 'disabled'})
+})

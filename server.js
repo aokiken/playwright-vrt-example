05836b5e6975cbfe1f9064f4af46eefbd33cdbe3
aokiken <aokiken@me.com>
2022-08-02 21:42:03 +0900

    テスト用サーバーを追加

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('node:http')
+
+const server = http.createServer()
+
+server.on('request', (request, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(`<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>こんにちは世界</h1>
+</body>
+</html>`)
+})
+
+server.listen(3000)
